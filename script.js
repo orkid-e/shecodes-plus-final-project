84feee029e86adace12bab5068949a36c81fa139
Orkide <mailorkide@gmail.com>
2025-01-05 20:14:36 +0000

    Updated JS

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
-function searchCity(event) {
+function newCity(event) {
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
     let h1 = document.querySelector("h1");
     h1.innerHTML = `${searchInput.value}`;
 }
-
     let searchFormElement = document.querySelector("#search-form");
-    searchFormElement.addEventListener ("submit", searchCity)
+    searchFormElement.addEventListener ("submit", newCity)
 
 function displayTemp(response) {
     let temperature = Math.round(response.data.temperature.current);
     console.log(response.data.temperature.current)
 }
-    let apiKey = "4c0ff2fd9eo01d24aa2b0f71ef013t53"
-    let apiUrl = "https://api.shecodes.io/weather/v1/current?query=london&key=4c0ff2fd9eo01d24aa2b0f71ef013t53";
-    axios.get(apiUrl).then(displayTemp);
 
+function updateWeather(response) {
+console.log(response.data);
+
+}
+function searchCity(city) {
+    let apiKey = "4c0ff2fd9eo01d24aa2b0f71ef013t53"
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=london&key=${apiKey}`;
+    axios.get(apiUrl).then(updateWeather);
+    console.log(apiKey);
+}
