365d2239aed753f5350f1a9b106fd2d942f6f973
Orkide <mailorkide@gmail.com>
2025-01-05 16:47:55 +0000

    Updated JS - Display Temp function in Console Log

--- script.js
+++ script.js
@@ -5,6 +5,14 @@ function searchCity(event) {
     h1.innerHTML = `${searchInput.value}`;
 }
 
+    let searchFormElement = document.querySelector("#search-form");
+    searchFormElement.addEventListener ("submit", searchCity)
 
-let searchFormElement = document.querySelector("#search-form");
-searchFormElement.addEventListener ("submit", searchCity)
+function displayTemp(response) {
+    let temperature = Math.round(response.data.temperature.current);
+    console.log(response.data.temperature.current)
+}
+    let apiKey = "4c0ff2fd9eo01d24aa2b0f71ef013t53"
+    let apiUrl = "https://api.shecodes.io/weather/v1/current?query=london&key=4c0ff2fd9eo01d24aa2b0f71ef013t53";
+    axios.get(apiUrl).then(displayTemp);
+
