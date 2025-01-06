fe787c775574ebff23e9e6004e747a1ec23ff3c4
Orkide <mailorkide@gmail.com>
2025-01-06 14:32:50 +0000

    Updated JS

--- script.js
+++ script.js
@@ -1,24 +1,22 @@
 function newCity(event) {
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
+    searchCity(searchInput.value);
+  }
+  let searchFormElement = document.querySelector("#search-form");
+  searchFormElement.addEventListener("submit", newCity);
+  
+  function displayTemp(response) {
     let h1 = document.querySelector("h1");
-    h1.innerHTML = `${searchInput.value}`;
-}
-    let searchFormElement = document.querySelector("#search-form");
-    searchFormElement.addEventListener ("submit", newCity)
-
-function displayTemp(response) {
+    h1.innerHTML = response.data.city;
+  
     let temperature = Math.round(response.data.temperature.current);
-    console.log(response.data.temperature.current)
-}
-
-function updateWeather(response) {
-console.log(response.data);
-
-}
-function searchCity(city) {
-    let apiKey = "4c0ff2fd9eo01d24aa2b0f71ef013t53"
-    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=london&key=${apiKey}`;
-    axios.get(apiUrl).then(updateWeather);
-    console.log(apiKey);
-}
+    let h2 = document.querySelector("h2");
+    h2.innerHTML = `${temperature}°C`;
+  }
+  
+  function searchCity(city) {
+    let apiKey = "4c0ff2fd9eo01d24aa2b0f71ef013t53";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+    axios.get(apiUrl).then(displayTemp);
+  }
